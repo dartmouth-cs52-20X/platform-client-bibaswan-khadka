9590be686fb86622b02154531f8776cfb29644f9
bibaswan-khadka <bibaswan.khadka.22@dartmouth.edu>
2020-07-16 01:38:40 -0500

    React support added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let num = 0;
+const App = () => <div className="test">All the REACT are belong to us!</div>;
 
-setInterval(() => {
-  num += 1;
-  $('#main').html(`You've been on this page for ${num} seconds`);
-}, 1000);
+ReactDOM.render(<App />, document.getElementById('main'));
